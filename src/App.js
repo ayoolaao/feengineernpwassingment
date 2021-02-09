0bf340282a88f8bdd21790339096f930c5a3769e
Ayoola Abimbola <ao.abimbola@gmail.com>
2021-02-09 14:45:19 -0600

    initial routing setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './screens/home/Home';
+import OrderDetails from './screens/orderDetails/OrderDetails';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Header</p>
       </header>
+
+      <Router>
+        <Switch>
+          <Route exact path='/order' render={props => <OrderDetails {...props}/>} />
+          <Route exact path='/' render={props => <Home {...props}/>} />
+        </Switch>
+      </Router>
     </div>
   );
 }
